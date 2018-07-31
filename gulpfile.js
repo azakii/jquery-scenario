4508fbd9a0b116a7022af38fba668b95cad7ec06
ahmed zaki <ah.zakii@hotmail.com>
2018-07-31 23:29:23 +0200

    upload test project

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var pkg = require('./package.json');
+
+// Copy third party libraries from /node_modules into /vendor
+gulp.task('vendor', function() {
+
+  // Bootstrap
+  gulp.src([
+      './node_modules/bootstrap/dist/**/*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+    ])
+    .pipe(gulp.dest('./vendor/bootstrap'))
+
+  // jQuery
+  gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery'))
+
+})
+
+// Default task
+gulp.task('default', ['vendor']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+gulp.task('less', function(){
+  return gulp.src('vendor/scss/*.scss')
+    .pipe(less()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('css'))
+});
+
+
+// Dev task
+gulp.task('dev', ['browserSync'], function() {
+  gulp.watch('./css/*.css', browserSync.reload);
+  gulp.watch('./*.html', browserSync.reload);
+});
